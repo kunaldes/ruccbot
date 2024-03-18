e0314381420ab34a9d4fcd1f403a45653cb33f5e
Kunal <k4.desai@gmail.com>
2024-03-17 20:07:00 -0700

    Split choose into its own file and add ordering.

---
+++ src/choices.rs
@@ -0,0 +1,58 @@
+use crate::{Context, Error};
+
+use lazy_static::lazy_static;
+use rand::{seq::SliceRandom, Rng};
+use regex::Regex;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"^\s*(\d+)\s*-\s*(\d+)\s*$").unwrap();
+}
+
+fn split_input(input: String) -> Vec<String> {
+    if input.contains(',') {
+        input
+            .split(',')
+            .skip_while(|x| x.is_empty())
+            .map(|x| x.trim().into())
+            .collect()
+    } else {
+        input.split_whitespace().map(|x| x.into()).collect()
+    }
+}
+
+fn do_choose(items: String) -> Option<String> {
+    if let Some(captures) = RE.captures(items.as_str()) {
+        let (_, [i1, i2]) = captures.extract();
+        if let (Ok(num1), Ok(num2)) = (i1.parse(), i2.parse()) {
+            let [min, max]: [i32; 2] = std::cmp::minmax(num1, num2);
+            return Some(rand::thread_rng().gen_range(min..=max).to_string());
+        }
+    }
+    let foo: Vec<String> = split_input(items);
+    foo.choose(&mut rand::thread_rng()).cloned()
+}
+
+fn do_order(items: String) -> Option<String> {
+    let mut foo = split_input(items);
+    if foo.is_empty() {
+        return None;
+    }
+    foo.shuffle(&mut rand::thread_rng());
+    Some(foo.join(", "))
+}
+
+#[poise::command(prefix_command, aliases("c"))]
+pub async fn choose(ctx: Context<'_>, #[rest] items: Option<String>) -> Result<(), Error> {
+    if let Some(choice) = do_choose(items.unwrap_or_default()) {
+        ctx.say(choice).await?;
+    }
+    Ok(())
+}
+
+#[poise::command(prefix_command, aliases("o"))]
+pub async fn order(ctx: Context<'_>, #[rest] items: Option<String>) -> Result<(), Error> {
+    if let Some(ordering) = do_order(items.unwrap_or_default()) {
+        ctx.say(ordering).await?;
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,11 @@
 #![feature(cmp_minmax)]
+mod choices;
 
-use lazy_static::lazy_static;
 use poise::serenity_prelude as serenity;
-use rand::{seq::SliceRandom, Rng};
-use regex::Regex;
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-
-lazy_static! {
-    static ref RE: Regex = Regex::new(r"^\s*(\d+)\s*-\s*(\d+)\s*$").unwrap();
-}
-
-fn split_input(input: String) -> Vec<String> {
-    if input.contains(',') {
-        input
-            .split(',')
-            .skip_while(|x| x.is_empty())
-            .map(|x| x.trim().into())
-            .collect()
-    } else {
-        input.split_whitespace().map(|x| x.into()).collect()
-    }
-}
-
-fn do_choose(choices: String) -> Option<String> {
-    if let Some(captures) = RE.captures(choices.as_str()) {
-        let (_, [i1, i2]) = captures.extract();
-        if let (Ok(num1), Ok(num2)) = (i1.parse(), i2.parse()) {
-            let [min, max]: [i32; 2] = std::cmp::minmax(num1, num2);
-            return Some(rand::thread_rng().gen_range(min..=max).to_string());
-        }
-    }
-    let foo: Vec<String> = split_input(choices);
-    foo.choose(&mut rand::thread_rng()).cloned()
-}
-
-#[poise::command(prefix_command, aliases("c"))]
-async fn choose(ctx: Context<'_>, #[rest] choices: Option<String>) -> Result<(), Error> {
-    if let Some(choice) = do_choose(choices.unwrap_or_default()) {
-        ctx.say(choice).await?;
-    }
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() {
@@ -53,9 +15,9 @@ async fn main() {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![choose()],
+            commands: vec![choices::choose(), choices::order()],
             prefix_options: poise::PrefixFrameworkOptions {
-                prefix: Some(String::from(".")),
+                prefix: Some(".".into()),
                 ..Default::default()
             },
             ..Default::default()
