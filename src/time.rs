5889fd005ee75081890f17c0a14f9b3019c45965
Kunal <k4.desai@gmail.com>
2025-04-08 01:27:16 -0700

    Don't panic if the world breaks.
    Surely we don't need to worry about the system clock breaking on us.

--- src/time.rs
+++ src/time.rs
@@ -5,10 +5,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 /// Return the current time in seconds since the Unix epoch.
 #[poise::command(slash_command)]
 pub async fn time(ctx: Context<'_>) -> Result<(), Error> {
-    let time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("negative time")
-        .as_secs();
+    let time = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
     ctx.send(
         poise::CreateReply::default()
             .content(time.to_string())
